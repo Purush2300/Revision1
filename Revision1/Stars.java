8a708f2d020286b040e1f5aa91b256088735dfd9
purush2300 <reddypurushothm@gmail.com>
2026-03-18 20:38:31 +0530

    first commit

---
+++ Revision1/Stars.java
@@ -0,0 +1,12 @@
+package Revision1;
+
+public class Stars {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            for (int j = 5; j >=i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
