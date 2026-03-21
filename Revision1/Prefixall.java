0c2770e6c3cbc53f295b5064d65e61224ec15bf1
purush2300 <reddypurushothm@gmail.com>
2026-03-21 22:00:37 +0530

    Initial commit

--- Revision1/Prefixall.java
+++ Revision1/Prefixall.java
@@ -66,19 +66,35 @@ public class Prefixall {
 
 // System.out.println(max);
 // _______________________________________________________________________________________
-int []a={10,5,2,6};
-int k=100;
-int left=0;
-int count=0;
-int mul=1;
-for (int right= 0; right < a.length; right++) {
-    mul*=a[right];
-    while(mul>=k){
-        mul/=a[left];
-        left++;
-    }
-    count+=(right-left+1);
+// int []a={10,5,2,6};
+// int k=100;
+// int left=0;
+// int count=0;
+// int mul=1;
+// for (int right= 0; right < a.length; right++) {
+//     mul*=a[right];
+//     while(mul>=k){
+//         mul/=a[left];
+//         left++;
+//     }
+//     count+=(right-left+1);
+// }
+// System.out.println(count);
+// ___________________________________________________________
+int []a={1,2,3,4,5,6,1};
+int k=3;
+int sum=0;
+int max=0;
+for (int i = 0; i <k; i++) {
+    sum+=a[i];
 }
-System.out.println(count);
+max=sum;
+int last=a.length-1;
+for (int i = k-1; i>=0; i--) {
+    sum=sum-a[i]+a[last];
+    last--;
+    max=Math.max(max, sum);
+}
+System.out.println(max);
     }
 }
